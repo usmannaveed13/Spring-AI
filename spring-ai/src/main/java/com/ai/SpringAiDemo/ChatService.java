36a0a4d09135ef7f6565b045cb652fe9b504801a
Usman Naveed <usman.naveed@2iqresearch.com>
2025-07-25 17:45:44 +0500

    adding chat service for openAI

---
+++ spring-ai/src/main/java/com/ai/SpringAiDemo/ChatService.java
@@ -0,0 +1,17 @@
+package com.ai.SpringAiDemo;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+    private final ChatModel chatModel;
+
+    public ChatService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String getResponse(String prompt) {
+        return chatModel.call(prompt);
+    }
+}
---
+++ spring-ai/src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -0,0 +1,20 @@
+package com.ai.SpringAiDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenAIController {
+
+    ChatService chatService;
+
+    public GenAIController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @GetMapping("ask-ai")
+    public String getResponse(@RequestParam String prompt){
+        return chatService.getResponse(prompt);
+    }
+}
