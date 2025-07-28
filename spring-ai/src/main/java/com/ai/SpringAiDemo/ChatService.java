8e9930fd11dd04f208ed9c1231a5dab0b41106b3
Usman Naveed <usman.naveed@2iqresearch.com>
2025-07-28 15:10:17 +0500

    creating a second api for using chatModel with option

--- spring-ai/src/main/java/com/ai/SpringAiDemo/ChatService.java
+++ spring-ai/src/main/java/com/ai/SpringAiDemo/ChatService.java
@@ -1,6 +1,9 @@
 package com.ai.SpringAiDemo;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +16,14 @@ public class ChatService {
 
     public String getResponse(String prompt) {
         return chatModel.call(prompt);
+    }
+
+    public String getResponseOptions(String prompt) {
+        OllamaOptions options = new OllamaOptions();
+        options.setTemperature(0.4);
+        options.setModel("mistral");
+
+        ChatResponse response = chatModel.call(new Prompt(prompt, options));
+        return response.getResult().getOutput().getText();
     }
 }
--- spring-ai/src/main/java/com/ai/SpringAiDemo/GenAIController.java
+++ spring-ai/src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -17,4 +17,9 @@ public class GenAIController {
     public String getResponse(@RequestParam String prompt){
         return chatService.getResponse(prompt);
     }
+
+    @GetMapping("ask-ai-options")
+    public String getResponseOptions(@RequestParam String prompt){
+        return chatService.getResponseOptions(prompt);
+    }
 }
