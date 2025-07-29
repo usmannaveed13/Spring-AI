69b4358fed03a02a7986391ab0e83399012da74b
Usman Naveed <usman.naveed@2iqresearch.com>
2025-07-29 11:05:55 +0500

    creating api for generating image from OpenAiImageModel

--- spring-ai/src/main/java/com/ai/SpringAiDemo/GenAIController.java
+++ spring-ai/src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -1,16 +1,22 @@
 package com.ai.SpringAiDemo;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class GenAIController {
 
-    ChatService chatService;
+    private final ChatService chatService;
+    private final ImageService imageService;
 
-    public GenAIController(ChatService chatService) {
+    public GenAIController(ChatService chatService, ImageService imageService) {
         this.chatService = chatService;
+        this.imageService = imageService;
     }
 
     @GetMapping("ask-ai")
@@ -21,5 +27,12 @@ public class GenAIController {
     @GetMapping("ask-ai-options")
     public String getResponseOptions(@RequestParam String prompt){
         return chatService.getResponseOptions(prompt);
+    }
+
+    @GetMapping("generate-image")
+    public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
     }
 }
---
+++ spring-ai/src/main/java/com/ai/SpringAiDemo/ImageService.java
@@ -0,0 +1,32 @@
+package com.ai.SpringAiDemo;
+
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+    private final OpenAiImageModel oPenAiImageModel;
+
+    public ImageService(OpenAiImageModel oPenAiImageModel) {
+        this.oPenAiImageModel = oPenAiImageModel;
+    }
+
+    public ImageResponse generateImage(String prompt) {
+//        ImageResponse imageResponse = oPenAiImageModel.call(
+//                new ImagePrompt(prompt));
+
+
+        ImageResponse imageResponse = oPenAiImageModel.call(
+                new ImagePrompt(prompt,
+                        OpenAiImageOptions.builder()
+                                .quality("hd")
+                                .N(4)
+                                .height(1024)
+                                .width(1024).build())
+        );
+        return imageResponse;
+    }
+}
