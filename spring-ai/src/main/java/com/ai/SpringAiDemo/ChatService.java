54203ccb6d5cf3952c6bd66df73b17cdf21dde37
Usman Naveed <usman.naveed@2iqresearch.com>
2025-07-30 16:25:43 +0500

    remove use of ollama and using the open AI & creating Recipe service

--- spring-ai/src/main/java/com/ai/SpringAiDemo/ChatService.java
+++ spring-ai/src/main/java/com/ai/SpringAiDemo/ChatService.java
@@ -3,7 +3,8 @@ package com.ai.SpringAiDemo;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.api.OllamaOptions;
+
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,11 +20,16 @@ public class ChatService {
     }
 
     public String getResponseOptions(String prompt) {
-        OllamaOptions options = new OllamaOptions();
-        options.setTemperature(0.4);
-        options.setModel("mistral");
+        ChatResponse response =  chatModel.call(
+               new Prompt(
+                       prompt,
+                       OpenAiChatOptions.builder()
+                               .model("gpt-4-0")
+                               .temperature(0.4)
+                               .build()
 
-        ChatResponse response = chatModel.call(new Prompt(prompt, options));
-        return response.getResult().getOutput().getText();
+               ));
+
+       return response.getResult().getOutput().getText();
     }
 }
--- spring-ai/src/main/java/com/ai/SpringAiDemo/GenAIController.java
+++ spring-ai/src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -14,10 +14,12 @@ public class GenAIController {
 
     private final ChatService chatService;
     private final ImageService imageService;
+    private final RecipeService recipeService;
 
-    public GenAIController(ChatService chatService, ImageService imageService) {
+    public GenAIController(ChatService chatService, ImageService imageService, RecipeService recipeService) {
         this.chatService = chatService;
         this.imageService = imageService;
+        this.recipeService = recipeService;
     }
 
     @GetMapping("ask-ai")
@@ -51,4 +53,12 @@ public class GenAIController {
 
         return imageUrls;
     }
+
+    @GetMapping("recipe-creator")
+    public String recipeCreator(@RequestParam String ingredients,
+                                      @RequestParam(defaultValue = "any") String cuisine,
+                                      @RequestParam(defaultValue = "any") String dietaryRestriction) {
+        return recipeService.createRecipe(ingredients, cuisine, dietaryRestriction);
+    }
+
 }
